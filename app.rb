df977b85a8b3a7ecb58623dcf1dd3cac673374bc
edensongjbs <edensongjbs@gmail.com>
2020-06-14 22:22:52 -0400

    Done.

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "Welcome to your app!!!! "
+    "Welcome to your app!!!! And some more text.  Loading with Shotgun"
   end
 
 end
